a55c0cfee327d02a2fbf9427a7d997bafc53a532
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-09 15:53:45 +0630

    signup api route

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,4 +1,41 @@
 import {Request, Response} from 'express'
+import { prismaClient } from '..'
+import {hashSync} from 'bcrypt'
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+} | null
+
+export const signUp = async (req: Request, res: Response) => {
+    const {name, email, password} = req.body
+    
+    let user: User = await prismaClient.user.findFirst({
+        where: {
+            email: email
+        }
+    })
+
+    if (user) {
+        throw Error('User already exists!')
+    }
+
+    user = await prismaClient.user.create({
+        data: {
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    })
+
+    const {password: pass, ...rest} = user
+
+    res.status(201).json(rest)
+}
 
 export const login = (req: Request, res: Response) => {
     res.json({message: "Login route is working..."})
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import {login} from '../controllers/AuthController'
+import {login, signUp} from '../controllers/AuthController'
 
 const authRoutes: Router = Router()
 
 authRoutes.get('/login', login)
+authRoutes.post('/signup', signUp)
 
 export default authRoutes
