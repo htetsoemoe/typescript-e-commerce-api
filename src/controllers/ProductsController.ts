631e04fc63db276e3e3042302f2039eb0bc30ef2
htetsoemoe <soemoehtet.mdy@gmail.com>
2024-05-15 16:10:59 +0630

    product crud api routes with admin role

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -1,5 +1,7 @@
 import {Request, Response} from "express"
 import {prismaClient} from ".."
+import { NotFoundException } from "../exceptions/not-found"
+import { ErrorCode } from "../exceptions/root"
 
 export const createProduct = async (req: Request, res: Response) => {
     const tags = await req.body.tags.join(",")
@@ -10,4 +12,59 @@ export const createProduct = async (req: Request, res: Response) => {
         }
     })
     res.status(201).json(product)
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const product = req.body
+        if (product.tags) {
+            product.tags = product.tags.join(',')
+        }
+        const updatedProduct = await prismaClient.product.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: product
+        })
+        res.status(200).json(updatedProduct)
+    } catch (error) {
+        throw new NotFoundException("Product Not Found!", ErrorCode.PRODUCT_NOT_FOUND)
+    }
+}
+
+export const listProducts = async (req: Request, res: Response) => {
+    const skip = parseInt(req.query.skip as string) || 0
+
+    const count = await prismaClient.product.count()
+    const products = await prismaClient.product.findMany({
+        skip: skip,
+        take: 10
+    })
+    res.status(200).json({count, data: products})
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        const product = await prismaClient.product.findFirstOrThrow({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        res.status(200).json(product)
+    } catch (error) {
+        throw new NotFoundException("Product Not Found!", ErrorCode.PRODUCT_NOT_FOUND)
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const deletedProduct = await prismaClient.product.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        })
+        res.status(200).json(deletedProduct)
+    } catch (error) {
+        throw new NotFoundException("Product Not Found!", ErrorCode.PRODUCT_NOT_FOUND)
+    }
 }
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import { errorHandler } from "../error-handler";
 import authMiddleware from "../middlewares/auth";
 import adminMiddleware from "../middlewares/admin";
-import {createProduct} from "../controllers/ProductsController"
+import {createProduct, deleteProduct, getProductById, listProducts, updateProduct} from "../controllers/ProductsController"
 
 const productRouter: Router = Router()
 
 productRouter.post("/create", [authMiddleware, adminMiddleware], errorHandler(createProduct))
+productRouter.put("/:id", [authMiddleware, adminMiddleware], errorHandler(updateProduct))
+productRouter.delete("/:id", [authMiddleware, adminMiddleware], errorHandler(deleteProduct))
+productRouter.get("/", [authMiddleware, adminMiddleware], errorHandler(listProducts))
+productRouter.get("/:id", [authMiddleware, adminMiddleware], errorHandler(getProductById))
 
 export default productRouter
